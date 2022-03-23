d5b1bbf95afa8c35fc387fb554b56e05d50ea65c
Ilya758 <subscriber6436@gmail.com>
2022-03-23 10:12:51 +0300

    feat: implemented required components Header, SearchItem, SearchResults

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

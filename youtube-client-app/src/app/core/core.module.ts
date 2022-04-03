d8b28bc8cf73cd87cff461b2bd3c6c85e9b8f68a
Ilya758 <subscriber6436@gmail.com>
2022-04-03 22:18:18 +0300

    fix: fixed declarations in CoreModule, fixed styles

--- youtube-client-app/src/app/core/core.module.ts
+++ youtube-client-app/src/app/core/core.module.ts
@@ -5,15 +5,21 @@ import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { FilteringBlockComponent } from './components/filtering-block/filtering-block.component';
+import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FilteringBlockComponent],
+  declarations: [
+    HeaderComponent,
+    FilteringBlockComponent,
+    NotFoundPageComponent,
+  ],
   imports: [CommonModule, CoreRoutingModule, FormsModule, SharedModule],
   exports: [
     FormsModule,
     HeaderComponent,
     FilteringBlockComponent,
     SharedModule,
+    NotFoundPageComponent,
   ],
 })
 export class CoreModule {}

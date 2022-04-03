ce357b312be5a21c6b1cc8fafb40e966066eacdb
Ilya758 <subscriber6436@gmail.com>
2022-04-03 22:15:35 +0300

    feat: implemented LoginPageComponent

--- youtube-client-app/src/app/auth/auth.module.ts
+++ youtube-client-app/src/app/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './pages/auth-routing.module';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, AuthRoutingModule],
+  declarations: [LoginPageComponent],
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}

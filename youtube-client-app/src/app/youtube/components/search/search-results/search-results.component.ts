c79885191ea05d9d947347217c6567fb6c0f1f45
Ilya758 <subscriber6436@gmail.com>
2022-04-07 21:32:47 +0300

    refactor: reduced layout, styles, logic in SearchItemComponent and SearchResultsComponent

--- youtube-client-app/src/app/youtube/components/search/search-results/search-results.component.ts
+++ youtube-client-app/src/app/youtube/components/search/search-results/search-results.component.ts
@@ -1,14 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { YoutubeService } from 'src/app/youtube/services/youtube.service';
-import { IItem } from '../../../models/search-item.model';
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
-  data!: IItem[];
-
   youtubeService: YoutubeService;
 
   constructor(youtubeService: YoutubeService) {
@@ -16,13 +13,6 @@ export class SearchResultsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const URL =
-      'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/angular/response.json';
-    fetch(URL)
-      .then((res) => res.json())
-      .then((data) => {
-        this.data = Object.values(data)[3] as IItem[];
-      })
-      .catch((err) => console.log(err));
+    this.youtubeService.resetCurrentVideoInfo();
   }
 }

cec607d5c89242bef225abcb328db0de1bd62bc3
Ilya758 <subscriber6436@gmail.com>
2022-04-08 21:20:12 +0300

    fix: fixed FlexDirective

--- youtube-client-app/src/app/shared/directives/flex.directive.ts
+++ youtube-client-app/src/app/shared/directives/flex.directive.ts
@@ -1,21 +1,22 @@
 import { Directive, ElementRef } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Directive({
   selector: '[appFlex]',
 })
 export class FlexDirective {
-  constructor(private elementRef: ElementRef, private router: Router) {
+  constructor(private elementRef: ElementRef) {
     this.setFlex();
   }
 
   setFlex = () => {
     const element = this.elementRef.nativeElement as HTMLElement;
 
-    setTimeout(() => {
-      this.router.url === '/404'
-        ? element.classList.add('main_align_center')
-        : element.classList.remove('main_align_center');
-    });
+    const matches = window.location.pathname.match(
+      /\/(login|videos)?/i
+    ) as RegExpMatchArray;
+
+    matches || matches[0] === '/'
+      ? element.classList.remove('main_align_center')
+      : element.classList.add('main_align_center');
   };
 }
